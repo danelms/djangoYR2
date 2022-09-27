fa81e227eda6df20b336168307a4a8806cec83f4
danelms <74295308+danelms@users.noreply.github.com>
2022-09-27 15:33:54 +0100

    Finished lecture example

--- djangoProject/firstApp/models.py
+++ djangoProject/firstApp/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 class Pet(models.Model):
     _sexChoices = [('M', 'Male'), ('F', 'Female')]
-    _name = models.CharField(_MAX_LENGTH=100)
-    _submitter = models.CharField(_MAX_LENGTH=100)
-    _species = models.CharField(_MAX_LENGTH=30)
-    _breed = models.CharField(_MAX_LENGTH=30, blank=True)
+    _name = models.CharField(max_length=100)
+    _submitter = models.CharField(max_length=100)
+    _species = models.CharField(max_length=30)
+    _breed = models.CharField(max_length=30, blank=True)
     _description = models.TextField()
-    _sex = models.CharField(_MAX_LENGTH=1, choices=_sexChoices, blank=True)
-    age = models.IntegerField()
+    _sex = models.CharField(max_length=1, choices=_sexChoices, blank=True)
+    _age = models.IntegerField()
     _submissionDate =models.DateTimeField()
     #Implements many to many relationship between Vaccine and Pet classes
     _vaccinations = models.ManyToManyField('Vaccine', blank = True)
 
 class Vaccine(models.Model):
-    _name = models.CharField(_MAX_LENGTH=50)
+    _name = models.CharField(max_length=50)
