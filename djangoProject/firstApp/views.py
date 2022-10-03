6891707a74d56942da8680d47e76107c8608c7bb
danelms <352577@edu.cwa.ac.uk>
2022-10-03 16:20:17 +0100

    progress
    dad

--- djangoProject/firstApp/views.py
+++ djangoProject/firstApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Pet
 
 def home(request):
@@ -8,4 +9,8 @@ def home(request):
     return render(request, 'home.html',{'pets':pets,})
 
 def pet_detail(request, pet_id):
-    return HttpResponse(F"<h1>Pet Number: {pet_id}</h1>")
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except:
+        raise Http404("Pet not found")
+    return render(request, 'pet_detail.html', {'pet':pet,})
