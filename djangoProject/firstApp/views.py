b855a3026373f58ca6a4f435511857b3d2eb26dd
danelms <352577@edu.cwa.ac.uk>
2022-10-03 10:17:14 +0100

    03/10

--- djangoProject/firstApp/views.py
+++ djangoProject/firstApp/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from .models import Pet
 
 def home(request):
-    return HttpResponse("<h1>This is the home page!</h1>")
+    #return HttpResponse("<h1>This is the home page!</h1>")
+    pets = Pet.objects.all()
+    return render(request, 'home.html',{'pets':pets,})
 
 def pet_detail(request, pet_id):
     return HttpResponse(F"<h1>Pet Number: {pet_id}</h1>")
